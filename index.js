48e1bd888e47785609df721555c2504c10de09d8
th4nhlam <lam73nk@gmail.com>
2023-06-22 11:09:39 +0700

    edit clock

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
-const dropdownangle = document.querySelector(".dropdown-angle");
-const dropdownhover = document.querySelector(".dropdown2-hover");
-const dropdown2 = document.querySelector(".dropdown2");
-const hoatdong = document.getElementById("hohoatdong");
-const dangki = document.getElementById("drdangki");
-const hd=document.getElementById("lihoatdong");
-const dt=document.getElementById("lidoitac");
-const lidangki = document.getElementById("lidangki");
 
-hd.addEventListener("click", ()=>{document.getElementById("hoatdong").scrollIntoView();});
-dt.addEventListener("click", ()=>{document.getElementById("sponsor").scrollIntoView();});
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
 })
 
-lihoatdong.addEventListener("click", () => {
-    lihoatdong.classList.toggle("active");
-    hoatdong.classList.toggle("active");
-})
-lidangki.addEventListener("click", () => {
-    lidangki.classList.toggle("active");
-    dangki.classList.toggle("active");
-})
-dropdownangle.addEventListener("click", () => {
-    // dropdownhover.classList.toggle("active");
-    
-    // console.log("Sfsdg");
-})
-dropdownangle.addEventListener("click", () => {
-    // dropdownhover[1].classList.toggle("active");
-    // hoatdong.toggle("active");
-    // console.log("Sfsdg");
-})
-
-document.querySelectorAll(".choose-box").forEach(n => n.addEventListener("click", () =>{
+document.querySelectorAll("link").forEach(n => n.addEventListener("click", () =>{
     hamburger.classList.remove("active");
     menu.classList.remove("active");
 }))
@@ -50,13 +21,9 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 
-const daymb=document.getElementById("day1");
-const hourmb=document.getElementById("hour1");
-const minmb=document.getElementById("minute1");
-const secmb=document.getElementById("second1");
 const currentYear = new Date().getFullYear();
 
-const DJFTime = new Date(`March 31 ${currentYear} 00:00:00`);
+const DJFTime = new Date(`March 30 ${currentYear} 00:00:00`);
 
 //Update countdown time
 function updateCountdown()
@@ -69,24 +36,10 @@ function updateCountdown()
     const m = Math.floor(diff / 1000 / 60) % 60;
     const s = Math.floor(diff / 1000) % 60;
 
-    days.innerHTML = '0' + d;
+    days.innerHTML = d;
     hours.innerHTML = h < 10 ? '0' + h : h;
     minutes.innerHTML = m < 10 ? '0' + m : m;
     seconds.innerHTML = s < 10 ? '0' + s : s;
-
-    daymb.innerHTML = '0' + d;
-    hourmb.innerHTML = h < 10 ? '0' + h : h;
-    minmb.innerHTML = m < 10 ? '0' + m : m;
-    secmb.innerHTML = s < 10 ? '0' + s : s;
 }
 
 setInterval(updateCountdown, 1000);
-
-// npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
-//
-
-
-
-
-
-
